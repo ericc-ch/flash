5bbde076482fccbe86685b6a83cea9e1a5ab4128
Erick Christian <erickchristian48@gmail.com>
2025-08-21 20:53:28 +0700

    chore: run formatter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-import '@fontsource-variable/inter'
-import '@fontsource-variable/jetbrains-mono'
+import "@fontsource-variable/inter"
+import "@fontsource-variable/jetbrains-mono"
 
-import './index.css'
-import App from './app'
+import "./index.css"
+import App from "./app"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./app/register"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
