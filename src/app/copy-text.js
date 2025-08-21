14e5b4a476ff763911b64e5717fbe9ae64ed5d7b
Erick Christian <erickchristian48@gmail.com>
2025-08-21 17:14:05 +0700

    refactor: migrate copy-text

---
+++ src/app/copy-text.js
@@ -0,0 +1,33 @@
+import { LitElement, html, unsafeCSS } from "lit"
+import globalCSS from "../index.css?inline"
+
+export class CopyText extends LitElement {
+  static properties = {
+    text: { type: String },
+  }
+
+  static styles = [unsafeCSS(globalCSS)]
+
+  constructor() {
+    super()
+    this.text = ""
+  }
+
+  async copyToClipboard() {
+    await navigator.clipboard.writeText(this.text)
+  }
+
+  render() {
+    return html`
+      <div class="relative text-sm">
+        <pre class="font-mono pt-12">${this.text}</pre>
+        <button
+          class="absolute top-2 right-2 bg-blue-600 hover:bg-blue-500 active:bg-blue-300 transition-colors text-white p-1 rounded-md cursor-pointer border-none"
+          @click=${this.copyToClipboard}
+        >
+          Copy
+        </button>
+      </div>
+    `
+  }
+}
